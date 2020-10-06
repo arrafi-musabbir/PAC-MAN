9f1a84f150716c89b55970b9a527425cc9547d45
arrafi-musabbir <arrafi-musabbir@github.com>
2020-10-06 23:29:28 +0600

    tracking player upto grid

--- PLAYER.py
+++ PLAYER.py
@@ -10,20 +10,31 @@ class Player:
         self.grid_pos = pos
         self.pix_pos = self.get_pix_pos()
         self.direction = vec(1, 0)
+        self.stored_direction = None
 
     def update(self):
         self.pix_pos += self.direction
+        if (int(self.pix_pos.x + iv.top_bottom_buffer // 2) %
+           self.Game.cell_width == 0):
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
+        if (int(self.pix_pos.y + iv.top_bottom_buffer // 2) %
+           self.Game.cell_height == 0):
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1):
+                if self.stored_direction is not None:
+                    self.direction = self.stored_direction
         self.grid_pos.x = (((self.pix_pos.x
-                           - iv.top_bottom_buffer+self.Game.cell_width//2)
-                           // self.Game.cell_width) + 0)
+                           - iv.top_bottom_buffer + self.Game.cell_width // 2)
+                            // self.Game.cell_width) - 0.1)
         self.grid_pos.y = (((self.pix_pos.y
-                           - iv.top_bottom_buffer+self.Game.cell_height//2)
-                           // self.Game.cell_height) + 0)
+                           - iv.top_bottom_buffer + self.Game.cell_height // 2)
+                            // self.Game.cell_height) + 0.2)
 
     def appear(self):
         pygame.draw.circle(self.Game.screen, iv.PLAYER_COLOUR,
                            (int(self.pix_pos.x), int(self.pix_pos.y)),
-                           self.Game.cell_width - 2)
+                           self.Game.cell_width - 5)
         pygame.draw.rect(self.Game.screen, iv.RED,
                          ((self.grid_pos.x * self.Game.cell_width)
                           + iv.top_bottom_buffer // 1.15,
@@ -32,7 +43,7 @@ class Player:
                           self.Game.cell_width, self.Game.cell_height), 1)
 
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
 
     def get_pix_pos(self):
         return vec((self.grid_pos.x * self.Game.cell_width)
